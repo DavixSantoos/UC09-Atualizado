b7ff93fc0b24e15d4df94b0ecf9d29494ba4c962
David C Santos <133790186+DavixSantoos@users.noreply.github.com>
2026-06-02 20:33:02 -0300

    Fornecedores

---
+++ TDS02-UC9-main/01/js/fornecedores/form.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form-fornecedor');
+ 
+async function salvarFornecedor() {
+ 
+    form.addEventListener('submit', async(e) => {
+        e.preventDefault();
+ 
+        const nome = document.getElementById('nomeFantasia').value
+        const cnpj = document.getElementById('cnpj').value
+ 
+        if (!nome || !cnpj) {
+            alert('Por favor, preencha todos os campos obrigatórios!');
+            return;
+        }
+       
+        const fornecedorDados = {
+            nomeFantasia: nome,
+            cnpj: cnpj,
+        }
+ 
+        const url = `${API_BASE_URL}/fornecedores`;
+ 
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(fornecedorDados)
+            });
+ 
+            if (!response.ok) throw new Error("Erro ao salvar fornecedor")
+ 
+            window.location.href = 'index.html';
+        } catch (error) {
+            console.error("Erro ao salvar: ", error);
+            alert('Erro ao salvar o fornecedor. Tente novamente!')
+        }
+ 
+    })
+}
+ 
+salvarFornecedor();
---
+++ TDS02-UC9-main/01/js/fornecedores/list.js
@@ -0,0 +1,29 @@
+async function carregarFornecedores() {
+    try {
+        const resposta = await fetch(`${API_BASE_URL}/fornecedores`)
+        const fornecedores = await resposta.json()
+
+        const tbody = document.getElementById("tabela-fornecedores")
+        tbody.innerHTML = '';
+
+        fornecedores.forEach(fornecedor => {
+            const novaLinhaDaTabela = document.createElement('tr')
+            novaLinhaDaTabela.innerHTML = `
+                <td>${fornecedor.id}</td>
+                <td>${fornecedor.nomeFantasia}</td>
+                <td>${fornecedor.cnpj}</td>
+                <td>
+                    <a href="./detalhes.html?id=${fornecedor.id}">Detalhes</a>
+                    <a href="#">Editar</a>
+                    <a href="#">Excluir</a>
+                </td>
+            `;
+            tbody.appendChild(novaLinhaDaTabela)
+        });
+        
+    } catch (error) {
+        console.error("Erro ao carregar fornecedores", error)
+    }
+}
+
+carregarFornecedores()
